f8ba26db70e9676e5733e27746b6f2c50a9ee779
Ravindra Thattahalli Javaraiah <c_rthat@codeaurora.org>
2014-08-27 11:30:29 +0530

    Display Supplementary Service Notification
    New interface added to support supplementary service notification
    messages.
    
    Conflicts:
    telecomm/java/android/telecom/Connection.java
    telecomm/java/android/telecom/ConnectionService.java
    telecomm/java/android/telecom/ParcelableCall.java
    Change-Id: I13dc423873e474b87f614ee2473bd997d6d48ffc
    
    Conflicts:
    	telecomm/java/android/telecom/ConnectionServiceAdapterServant.java

--- telecomm/java/android/telecom/Call.java
+++ telecomm/java/android/telecom/Call.java
@@ -544,6 +544,8 @@ public final class Call {
     private String mRemainingPostDialSequence;
     private InCallService.VideoCall mVideoCall;
     private Details mDetails;
+    private int mNotificationType;
+    private int mNotificationCode;
 
     /**
      * Obtains the post-dial sequence remaining to be emitted by this {@code Call}, if any.
@@ -553,6 +555,16 @@ public final class Call {
      */
     public String getRemainingPostDialSequence() {
         return mRemainingPostDialSequence;
+    }
+
+    /** @hide */
+    public int getNotificationType() {
+        return mNotificationType;
+    }
+
+    /** @hide */
+    public int getNotificationCode() {
+        return mNotificationCode;
     }
 
     /**
@@ -823,6 +835,9 @@ public final class Call {
         if (detailsChanged) {
             mDetails = details;
         }
+
+        mNotificationType = parcelableCall.getNotificationType();
+        mNotificationCode = parcelableCall.getNotificationCode();
 
         boolean cannedTextResponsesChanged = false;
         if (mCannedTextResponses == null && parcelableCall.getCannedSmsResponses() != null
